19137f4835343547943947724f07ba9378fa1df7
scokavci <scokavci@gmail.com>
2020-02-29 15:51:10 -0500

    update

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -6,6 +6,7 @@ public class LoopPractice {
         //some random code
         for(int i=0;i<10;i++){
             System.out.println("Counter :: "+i);
+            System.out.println("New modification");
         }
     }
 }
